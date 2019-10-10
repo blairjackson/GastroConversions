d3266795d2fb64fb7c18bf1e33a303629e44f536
blairjackson <blairgjackson@gmail.com>
2019-10-10 15:58:53 +0100

    Updated drop folder

--- sftp.py
+++ sftp.py
@@ -3,7 +3,7 @@ import pysftp
 
 def upload(host, username, password, file):
     with pysftp.Connection(host, username=username, password=password) as sftp:
-        with sftp.cd('/default/misc/'):  # temporarily chdir to public
+        with sftp.cd('/default/phone-uservisits/'):  # temporarily chdir to public
             print("uploading...." + host, username, password )
             print("uploading via SFTP.....")
             sftp.put(file)  # upload file to public/ on remote
