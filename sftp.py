7dcc08b997570a0a06e90aeb034c7b576d6cc56c
blairjackson <blairgjackson@gmail.com>
2020-03-06 10:29:09 +0000

    remove print for credentials

--- sftp.py
+++ sftp.py
@@ -4,7 +4,7 @@ import pysftp
 def upload(host, username, password, file):
     with pysftp.Connection(host, username=username, password=password) as sftp:
         with sftp.cd('/default/phone-uservisits/'):  # temporarily chdir to public
-            print("uploading...." + host, username, password )
+            #print("uploading...." + host, username, password )
             print("uploading via SFTP.....")
             sftp.put(file)  # upload file to public/ on remote
             print("successfully dropped to "+ username)
